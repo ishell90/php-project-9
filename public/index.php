07051866870c3ad81fc676fcbc94737f895c39e8
Илья Шалаев <alex.280200@mail.ru>
2023-09-28 17:21:35 +0300

    Разбираюсь с версткой начальнйо страницы

--- public/index.php
+++ public/index.php
@@ -10,12 +10,20 @@ if (file_exists($autoloadPath1)) {
 }
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
+    return $this->get('renderer')->render($response, 'index.phtml');
 });
+
 $app->run();
